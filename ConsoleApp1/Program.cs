19b40f84ca7f1c5cb31814cc6aef423fe2cca319
adras <adrasx@gmail.com>
2023-03-18 18:17:50 +0100

    Testapp showing some output

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace ConsoleApp1
+﻿using LogicHelperLib;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(LogicHelper.True);
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(LogicHelper.IsMaybeTrue(true));
+            }
+            Console.WriteLine("");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(LogicHelper.IsMaybeFalse(true));
+            }
+            Console.WriteLine("");
         }
     }
 }
--- LogicHelper/LogicHelper.cs
+++ LogicHelper/LogicHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LogicHelper
+namespace LogicHelperLib
 {
     /// <summary>
     /// Helper class to ease use of boolean comparisons.
